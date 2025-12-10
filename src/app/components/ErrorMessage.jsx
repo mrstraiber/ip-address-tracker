afe40c20426ea0557012235f742854b55aab8bf4
Souliman Terrah <souliman.terrah@hotmail.com>
2025-12-10 22:16:57 +0100

    added a children props to the Error message

--- src/app/components/ErrorMessage.jsx
+++ src/app/components/ErrorMessage.jsx
@@ -1,11 +1,8 @@
-export default function ErrorMessage({ handleCloseButton }) {
+export default function ErrorMessage({ handleCloseButton, children }) {
   return (
     <div className="relative mx-auto mt-5 w-fit rounded bg-white max-sm:mx-4">
       <div className="flex justify-center p-3 pt-6">
-        <h4 className="font-semibold text-red-500">
-          Invalid IP address. Please enter a valid IP address. (For example:
-          203.0.113.14)
-        </h4>
+        <h4 className="font-semibold text-red-500">{children}</h4>
       </div>
       <span
         onClick={handleCloseButton}
