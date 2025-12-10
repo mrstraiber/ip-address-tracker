eafb61df0af54bf0b4341cec563e084837531b58
Souliman Terrah <souliman.terrah@hotmail.com>
2025-12-10 01:04:37 +0100

    fix ErrorMessage components style

--- src/app/components/ErrorMessage.jsx
+++ src/app/components/ErrorMessage.jsx
@@ -1,10 +1,18 @@
-export default function ErrorMessage() {
+export default function ErrorMessage({ handleCloseButton }) {
   return (
-    <div>
-      <h4 className="m-auto mt-5 flex w-fit justify-center rounded-xl bg-white p-2 font-semibold text-red-400">
-        Invalid IP address. Please enter a valid IP address. (For example:
-        203.0.113.14)
-      </h4>
+    <div className="relative mx-auto mt-5 w-fit rounded bg-white max-sm:mx-4">
+      <div className="flex justify-center p-3 pt-6">
+        <h4 className="font-semibold text-red-500">
+          Invalid IP address. Please enter a valid IP address. (For example:
+          203.0.113.14)
+        </h4>
+      </div>
+      <span
+        onClick={handleCloseButton}
+        className="absolute top-1 right-0 h-5 w-5 cursor-pointer font-semibold text-black"
+      >
+        X
+      </span>
     </div>
   );
 }
