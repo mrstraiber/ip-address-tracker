75fad5af055d1ec9a86abde34102448761ed4e79
Souliman Terrah <souliman.terrah@hotmail.com>
2025-12-14 23:36:06 +0100

    i add a (LINK) components to the header

--- src/app/components/header.jsx
+++ src/app/components/header.jsx
@@ -1,9 +1,11 @@
+import Link from "next/link";
+
 export default function Header() {
   return (
     <header className="pt-8">
       <div className="text-center">
         <h3 className="text-3xl font-semibold text-blue-50">
-          IP Address Tracker
+          <Link href="/">IP Address Tracker</Link>
         </h3>
       </div>
     </header>
